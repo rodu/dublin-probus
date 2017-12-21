c2639bb374c743fc460766fe1cf0460c14c6323d
rodu <greenheron35@gmail.com>
2017-12-21 15:33:10 +0000

    Caches stops list in service

--- src/services/StopListService.js
+++ src/services/StopListService.js
@@ -1,9 +1,22 @@
 class StopListService {
+  constructor() {
+    this.stopList = null;
+  }
+
   get() {
+    if (this.stopList) {
+      return Promise.resolve(this.stopList);
+    }
+
     return new Promise((resolve, reject) => {
       fetch(`${process.env.PUBLIC_URL}/api/busstopinformation.json`)
         .then((response) => response.json())
         .then((data) => data.results)
+        .then((stopList) => {
+          this.stopList = stopList;
+
+          return stopList;
+        })
         .then(resolve)
         .catch(reject);
     });
